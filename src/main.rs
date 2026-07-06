5f208d248cd8893d919e6c40167148a352d3e4ae
Robin kron <krobin.it@gmail.com>
2026-07-06 13:10:37 +0200

    main refactored after separation

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,48 @@
-use color_eyre::eyre::Result;
-use crossterm::{
-    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
-};
-use ratatui::{
-    DefaultTerminal, Frame, Terminal,
-    backend::CrosstermBackend,
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    style::{Color, Modifier, Style},
-    symbols::border,
-    text::{Line, Span, Text},
-    widgets::{Block, Borders, List, ListItem, Paragraph, Widget},
-};
-use std::io;
+pub mod app;
+pub mod event;
+pub mod tui;
+pub mod ui;
+pub mod update;
 
-#[derive(Default)]
-struct App {
-    title: String,
-    quit: bool,
+use app::App;
+use color_eyre::eyre::Result;
+use event::{Event, EventHandler};
+use ratatui::crossterm::terminal::enable_raw_mode;
+use ratatui::{Terminal, backend::CrosstermBackend};
+use tui::Tui;
+use update::update;
+
+pub enum CurrentScreen {
+    Main,
+    Editing,
+    Exiting,
 }
 
-#[derive(Clone)]
-struct Task {
-    text: String,
-    done: bool,
+pub enum CurrentlyEditing {
+    Key,
+    Value,
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let mut terminal = ratatui::init();
-    let app_result = App::default().run(&mut terminal);
-    ratatui::restore();
-    app_result
-}
+    enable_raw_mode()?;
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
+    let mut app = App::new();
+    let terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.title.to_string().yellow(),
-        ])]);
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-}
+    tui.enter()?;
 
-impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        while !self.quit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
+    while !app.quit {
+        tui.draw(&mut app)?;
+        match tui.events.next()? {
+            Event::Tick => {}
+            Event::Key(key_event) => update(&mut app, key_event),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
         };
-        Ok(())
     }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.quit(),
-            _ => {}
-        }
-    }
-
-    fn quit(&mut self) {
-        self.quit = true;
-    }
+    tui.exit()?;
+    Ok(())
 }
