7d12e9026c2d6ba6a8ff6bc27134e8640adaa9fc
Robin kron <krobin.it@gmail.com>
2026-07-10 14:25:21 +0200

    Render time left in tasklist

--- src/widgets/task.rs
+++ src/widgets/task.rs
@@ -38,19 +38,48 @@ impl Task {
     pub fn as_list_item(&self) -> ListItem<'_> {
         let checkbox = if self.done { "[x]" } else { "[ ]" };
 
-        let style = if self.done {
-            Style::default().fg(Color::Green)
-        } else {
-            Style::default()
-        };
-
-        let content = Line::from(vec![
-            Span::styled(checkbox, style),
+        let mut spans = vec![
+            Span::styled(
+                checkbox,
+                Style::default().fg(if self.done {
+                    Color::Green
+                } else {
+                    Color::White
+                }),
+            ),
             Span::raw(" "),
             Span::raw(&self.text),
-        ]);
+        ];
 
-        ListItem::new(content)
+        if let Some(reminder) = self.reminder {
+            let now = Local::now();
+            let duration = reminder.signed_duration_since(now);
+
+            let reminder_text = if self.done {
+                "".to_string()
+            } else if duration.num_days() > 0 {
+                format!(" (in {}d)", duration.num_days())
+            } else if duration.num_hours() > 0 {
+                format!(" (in {}h)", duration.num_hours())
+            } else if duration.num_minutes() > 0 {
+                format!(" (in {}m)", duration.num_minutes())
+            } else {
+                " (now!)".to_string()
+            };
+
+            let reminder_style = if !self.done && duration.num_hours() <= 24 {
+                Style::default().fg(Color::Red)
+            } else if !self.done && duration.num_days() <= 7 {
+                Style::default().fg(Color::Yellow)
+            } else {
+                Style::default().fg(Color::LightCyan)
+            };
+
+            spans.push(Span::raw(" "));
+            spans.push(Span::styled(reminder_text, reminder_style));
+        }
+
+        ListItem::new(Line::from(spans))
     }
 }
 
