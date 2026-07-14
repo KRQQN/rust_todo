a108584a48385f7b9ec5ca1f39eda3ffcc18a02c
Robin kron <krobin.it@gmail.com>
2026-07-14 10:05:41 +0200

    Weekly completion stats

--- src/stats.rs
+++ src/stats.rs
@@ -1,4 +1,5 @@
-use chrono::Local;
+use chrono::{DateTime, Datelike, Duration, Local, NaiveDate};
+use crossterm::terminal::window_size;
 use ratatui::prelude::*;
 use ratatui::widgets::Paragraph;
 use ratatui::{
@@ -12,10 +13,13 @@ use ratatui::{
 use crate::widgets::task::Task;
 
 pub struct TaskStats {
-    pending_task_count: u32,
-    overdue_completed_task_count: u32,
-    completed_task_count: u32,
-    overdue_pending_task_count: u32,
+    pending_task_count: i32,
+    overdue_completed_task_count: i32,
+    completed_task_count: i32,
+    overdue_pending_task_count: i32,
+    this_week_completed: i32,
+    due_this_week: i32,
+    this_week_completed_late: i32,
 }
 
 impl TaskStats {
@@ -25,6 +29,9 @@ impl TaskStats {
             completed_task_count: 0,
             overdue_completed_task_count: 0,
             overdue_pending_task_count: 0,
+            this_week_completed: 0,
+            this_week_completed_late: 0,
+            due_this_week: 0,
         }
     }
 
@@ -48,17 +55,41 @@ impl TaskStats {
         // LEFT INNER SECTION
         let left_stats = vec![
             Line::from(vec![
-                Span::styled("Total Tasks: ", Style::default().fg(Color::White)),
+                Span::styled("This Week: ", Style::default().fg(Color::White))
+                    .add_modifier(Modifier::BOLD),
+            ]),
+            Line::from(vec![
+                Span::styled(" └─ Completed: ", Style::default().fg(Color::DarkGray)),
                 Span::styled(
-                    (self.completed_task_count + self.pending_task_count).to_string(),
+                    self.this_week_completed.to_string(),
                     Style::default()
-                        .fg(Color::Cyan)
+                        .fg(Color::Green)
                         .add_modifier(Modifier::BOLD),
+                ),
+            ]),
+            Line::from(vec![
+                Span::styled(" └─ Pending:   ", Style::default().fg(Color::DarkGray)),
+                Span::styled(
+                    self.due_this_week.to_string(),
+                    Style::default()
+                        .fg(Color::Yellow)
+                        .add_modifier(Modifier::BOLD),
+                ),
+            ]),
+            Line::from(vec![
+                Span::styled(" └─ Overdued: ", Style::default().fg(Color::DarkGray)),
+                Span::styled(
+                    self.this_week_completed_late.to_string(),
+                    Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
                 ),
             ]),
             Line::from(vec![Span::styled(" ", Style::default())]),
             Line::from(vec![
-                Span::styled("Completed: ", Style::default().fg(Color::White)),
+                Span::styled("Total: ", Style::default().fg(Color::White))
+                    .add_modifier(Modifier::BOLD),
+            ]),
+            Line::from(vec![
+                Span::styled(" └─ Completed: ", Style::default().fg(Color::DarkGray)),
                 Span::styled(
                     self.completed_task_count.to_string(),
                     Style::default()
@@ -67,15 +98,7 @@ impl TaskStats {
                 ),
             ]),
             Line::from(vec![
-                Span::styled(" └─ Overdue: ", Style::default().fg(Color::DarkGray)),
-                Span::styled(
-                    self.overdue_completed_task_count.to_string(),
-                    Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
-                ),
-            ]),
-            Line::from(vec![Span::styled(" ", Style::default())]),
-            Line::from(vec![
-                Span::styled("Pending:   ", Style::default().fg(Color::White)),
+                Span::styled(" └─ Pending: ", Style::default().fg(Color::DarkGray)),
                 Span::styled(
                     self.pending_task_count.to_string(),
                     Style::default()
@@ -92,7 +115,7 @@ impl TaskStats {
             ]),
             Line::from(vec![Span::styled(" ", Style::default())]),
             Line::from(vec![
-                Span::styled("Completion: ", Style::default().fg(Color::White)),
+                Span::styled("Completion rate: ", Style::default().fg(Color::White)),
                 Span::styled(
                     format!("{}%", self.completion_rate()),
                     Style::default()
@@ -130,34 +153,38 @@ impl TaskStats {
         frame.render_widget(right_paragraph, layout[1]);
     }
 
-    pub fn get_pending_count(&self) -> u32 {
+    pub fn get_pending_count(&self) -> i32 {
         self.pending_task_count
     }
 
-    pub fn get_completed_count(&self) -> u32 {
+    pub fn get_completed_count(&self) -> i32 {
         self.completed_task_count
     }
 
-    pub fn get_overdue_count(&self) -> u32 {
+    pub fn get_overdue_count(&self) -> i32 {
         self.overdue_pending_task_count
     }
 
-    fn overdue_rate(&self) -> u32 {
+    fn overdue_rate(&self) -> i32 {
         if self.pending_task_count == 0 {
             0
         } else {
             ((self.overdue_pending_task_count as f32 / self.pending_task_count as f32) * 100.0)
-                as u32
+                as i32
         }
     }
 
-    fn completion_rate(&self) -> u32 {
+    fn completion_rate(&self) -> i32 {
         let total = self.completed_task_count + self.pending_task_count;
         if total == 0 {
             0
         } else {
-            (self.completed_task_count as f32 / total as f32 * 100.0) as u32
+            (self.completed_task_count as f32 / total as f32 * 100.0) as i32
         }
+    }
+
+    fn is_this_week(&self, date: DateTime<Local>) -> bool {
+        date.iso_week() == Local::now().iso_week()
     }
 
     pub fn sync_task_stats(&mut self, tasklist: &Vec<Task>) {
@@ -165,16 +192,31 @@ impl TaskStats {
         self.completed_task_count = 0;
         self.overdue_pending_task_count = 0;
         self.overdue_completed_task_count = 0;
+        self.this_week_completed = 0;
+        self.due_this_week = 0;
 
         for task in tasklist {
             if task.done {
                 self.completed_task_count += 1;
 
-                if task.completed_at > task.reminder {
-                    self.overdue_completed_task_count += 1;
+                if task.completed_at.is_some_and(|dt| self.is_this_week(dt)) {
+                    self.this_week_completed += 1;
+                }
+
+                if let (Some(completed_at), Some(reminder)) = (task.completed_at, task.reminder) {
+                    if self.is_this_week(completed_at)
+                        && self.is_this_week(reminder)
+                        && completed_at > reminder
+                    {
+                        self.this_week_completed_late += 1;
+                    }
                 }
             } else {
                 self.pending_task_count += 1;
+
+                if task.reminder.is_some_and(|dt| self.is_this_week(dt)) {
+                    self.due_this_week += 1;
+                }
 
                 if let Some(rem) = &task.reminder {
                     if rem < &Local::now() {
